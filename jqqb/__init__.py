3c6731caa33fea8633923cc6badf632f885a05e2
Liorïnco <loic.tangre@connecting-food.com>
2023-02-23 19:12:00 +0100

    NG-1448: Refactor jqqb to manage aggregators

--- jqqb/__init__.py
+++ jqqb/__init__.py
@@ -1,19 +1,23 @@
 from typing import Union
 
-from jqqb.rule_group import RuleGroup
+from jqqb.rule import RuleInterface
+from jqqb.rule_constructor_from_json import RuleConstructorFromJson
 
 
 class QueryBuilder:
-    def __init__(self, rule_group: RuleGroup):
-        self.rule_group = rule_group
+    RULE_CONSTRUCTOR_FROM_JSON = RuleConstructorFromJson()
+
+    def __init__(self, rule: RuleInterface):
+        self.rule = rule
 
     @classmethod
-    def create_query_builder_from_json(
-        cls, rule_set_json: Union[str, dict]
+    def create_from_json(
+        cls, json_entry: Union[str, dict]
     ) -> "QueryBuilder":
+        """Construct a QueryBuilder instance from a given JSON."""
         return cls(
-            rule_group=RuleGroup.create_rule_group_from_json(
-                rule_group_json=rule_set_json
+            rule=cls.RULE_CONSTRUCTOR_FROM_JSON.create_rule_from_json(
+                json_entry=json_entry
             )
         )
 
@@ -23,15 +27,4 @@ class QueryBuilder:
         ]
 
     def object_matches_rules(self, object: dict) -> dict:
-        return self.rule_group.evaluate(object)
-
-    def inspect_objects(self, objects: list[dict]) -> list[dict]:
-        return [
-            {
-                "object": object,
-                "predicate": self.rule_group.get_predicate(object=object),
-                "results": self.rule_group.inspect(object=object),
-                "rules": self.rule_group.jsonify(object=object),
-                "selected": self.object_matches_rules(object=object),
-            } for object in objects
-        ]
+        return self.rule.evaluate(object)
--- jqqb/input.py
+++ jqqb/input.py
@@ -1,7 +1,7 @@
-import json
 from datetime import date, datetime, time
-from functools import reduce
-from typing import Any, Optional, Union
+from typing import Any, Optional
+
+from jqqb.helpers import get_object_item
 
 
 class Input:
@@ -30,62 +30,22 @@ class Input:
         self.type = type
         self.value = value
 
-    @classmethod
-    def create_input_from_json(cls, input_json: Union[dict, str]) -> "Input":
-        """Construct an Input instance from a given JSON.
-
-        Note:
-            JSON format:
-            ```
-            {
-                "field": string|null,
-                "type": string,
-                "value": number|string|null
-            }
-            ```
-        """
-        parsed_input_json = (
-            json.loads(input_json)
-            if isinstance(input_json, str)
-            else input_json
-        )
-        field = parsed_input_json["field"]
-        data = {"field": field, "type": parsed_input_json["type"]}
-
-        if not field:
-            data["value"] = parsed_input_json["value"]
-
-        return cls(**data)
-
     def get_value(self, object: Optional[dict] = None) -> Any:
-        if self.value is self.NotRetrievedValue and object is not None:
-            self.value = self.get_value_from_object(object=object)
-
-        return self.typecast_value(value_to_cast=self.value)
-
-    def get_value_from_object(self, object: dict) -> Any:
-        fields = self.field.split(".")
-        # TODO: Allow to choose how to browse a list
-        # last_object = reduce(lambda x, y: x.get(y, {}), fields[:-1], object)
-        last_object = reduce(
-            lambda x, y: x[0].get(y, {})
-            if isinstance(x, list)
-            else x.get(y, {}),
-            fields[:-1],
-            object,
-        )
-        return (
-            last_object[0].get(fields[-1])
-            if isinstance(last_object, list)
-            else last_object.get(fields[-1])
+        return self.typecast_value(
+            value_to_cast=get_object_item(object=object, field=self.field)
+            if self.field else self.value
         )
 
     def jsonify(self, object: dict) -> dict:
-        return {
-            "field": self.field,
-            "type": self.type,
-            "value": self.get_value(object=object),
-        }
+        json_result = {"type": self.type}
+
+        if self.field:
+            json_result["field"] = self.field
+
+        if self.value is not self.NotRetrievedValue:
+            json_result["value"] = self.value
+
+        return json_result
 
     def typecast_value(self, value_to_cast: Any) -> Any:
         cast_function = self._CAST_FUNCTIONS.get(self.type)

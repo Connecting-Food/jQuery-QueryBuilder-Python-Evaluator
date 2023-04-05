8225e9445915ab15b9f2166e611b0382e40b5280
Liorïnco <loic_tangre9@hotmail.fr>
2023-04-05 19:09:18 +0200

    Release: Upgrade version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jqqb",
-    version="0.1.5",
+    version="0.2.0",
     author="Connecting Food",
     author_email="developers@connecting-food.com",
     description="Python parsing, evaluation and inspection tools "
